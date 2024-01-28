ffa9e98c5764bec510a96c7ee48d260197478a50
ShampaShaw <shampashaw088@gmail.com>
2024-01-28 23:54:04 +0530

    chart

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import './home.css'
 import { FeaturedInfo } from '../../component/FeaturedInfo/FeaturedInfo'
+import { Chart } from '../../component/chart/Chart'
 
 export default function Home() {
   return (
     <div className='home'>
         <FeaturedInfo/>
+        <Chart/>
     </div>
   )
 }
