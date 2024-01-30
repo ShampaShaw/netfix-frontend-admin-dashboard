55d90c12d74365188f2720437deb7250d7ae96b0
ShampaShaw <shampashaw088@gmail.com>
2024-01-30 20:50:23 +0530

    widget

--- src/component/widgetLg/WidgetLg.jsx
+++ src/component/widgetLg/WidgetLg.jsx
@@ -2,7 +2,64 @@ import React from 'react'
 import './widgetLg.css'
 
 export const WidgetLg = () => {
+  const Button = ({type}) => {
+    return <button className={'widgetLgButton ' + type}>{type}</button>
+  }
   return (
-    <div>WidgetLg</div>
+    <div className='widgetLg'>
+      <h3 className='widgetLgTitle'>Latest Transactions</h3>
+      <table className='widgetLgTable'>
+        <tr className='widgetLgTr'>
+          <th className='widgetLgTh'>Customer</th>
+          <th className='widgetLgTh'>Date</th>
+          <th className='widgetLgTh'>Amount</th>
+          <th className='widgetLgTh'>Status</th>
+        </tr>
+        <tr className='widgetLgTr'>
+          <td className='widgetLgUser'>
+            <img src='https://images.pexels.com/photos/1526814/pexels-photo-1526814.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500' alt='' className='widgetLgImg'/>
+            <span className='widgetLgName'>Susan Carol</span>
+          </td>
+          <td className='widgetLgData'>30 Jan 2023</td>
+          <td className='widgetLgAmount'>$400.14</td>
+          <td className='widgetLgStatus'>
+            <Button type='Approved'/>
+          </td>
+        </tr>
+        <tr className='widgetLgTr'>
+          <td className='widgetLgUser'>
+            <img src='https://images.pexels.com/photos/1526814/pexels-photo-1526814.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500' alt='' className='widgetLgImg'/>
+            <span className='widgetLgName'>Susan Carol</span>
+          </td>
+          <td className='widgetLgData'>30 Jan 2023</td>
+          <td className='widgetLgAmount'>$400.14</td>
+          <td className='widgetLgStatus'>
+            <Button type='Declined'/>
+          </td>
+        </tr>
+        <tr className='widgetLgTr'>
+          <td className='widgetLgUser'>
+            <img src='https://images.pexels.com/photos/1526814/pexels-photo-1526814.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500' alt='' className='widgetLgImg' />
+            <span className='widgetLgName'>Susan Carol</span>
+          </td>
+          <td className='widgetLgData'>30 Jan 2023</td>
+          <td className='widgetLgAmount'>$400.14</td>
+          <td className='widgetLgStatus'>
+            <Button type='Pending'/>
+          </td>
+        </tr>
+        <tr className='widgetLgTr'>
+          <td className='widgetLgUser'>
+            <img src='https://images.pexels.com/photos/1526814/pexels-photo-1526814.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500' alt='' className='widgetLgImg'/>
+            <span className='widgetLgName'>Susan Carol</span>
+          </td>
+          <td className='widgetLgData'>30 Jan 2023</td>
+          <td className='widgetLgAmount'>$400.14</td>
+          <td className='widgetLgStatus'>
+            <Button type='Approved'/>
+          </td>
+        </tr>
+      </table>
+    </div>
   )
 }
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -3,13 +3,18 @@ import './home.css'
 import { FeaturedInfo } from '../../component/FeaturedInfo/FeaturedInfo'
 import { Chart } from '../../component/chart/Chart'
 import { userData } from '../../dummyData'
+import { WidgetSm } from '../../component/widgetSm/WidgetSm'
+import { WidgetLg } from '../../component/widgetLg/WidgetLg'
 
 export default function Home() {
   return (
     <div className='home'>
         <FeaturedInfo/>
         <Chart data={userData} title="User Analytics" grid dataKey="Active User"/>
-        <div className='homeWidgets'></div>
+        <div className='homeWidgets'>
+          <WidgetSm/>
+          <WidgetLg/>
+        </div>
     </div>
   )
 }
